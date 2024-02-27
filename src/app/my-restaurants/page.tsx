6c4c158884f2023d81689ba6b14ce43afd5af06c
Cristina RP <cristinarp95@gmail.com>
2024-02-27 13:48:29 +0100

    Add pages routes

---
+++ src/app/my-restaurants/page.tsx
@@ -0,0 +1,7 @@
+export default async function MyRestaurants() {
+  return (
+    <main>
+      <h1>MY FAVS</h1>
+    </main>
+  );
+}
---
+++ src/app/restaurants/page.tsx
@@ -0,0 +1,7 @@
+export default async function Restaurants() {
+  return (
+    <main>
+      <h1>RESTAURANTS</h1>
+    </main>
+  );
+}
