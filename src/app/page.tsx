cd7ab72a0cbc0c276e01c469e69a5ecb0109454d
Cristina RP <cristinarp95@gmail.com>
2024-02-27 20:03:32 +0100

    Design home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,47 @@
+'use client';
+
+import { Carousel } from '@/components/Carousel';
+import { StyledLink } from '@/components/StyledLink';
+import { Box, Button, Stack, Typography } from '@mui/material';
+
 export default function Home() {
   return (
     <main>
-      <h1>HOME</h1>
-      <h3>Ready for a top tasting experience?</h3>
-      <h3>What are you up for today? Choose cuisine type | Choose district</h3>
+      <Stack
+        direction={{ xs: 'column', sm: 'column', md: 'row' }}
+        spacing={{ xs: 1, sm: 1, md: 2 }}
+        width="100%"
+        margin="5% 5% 2% 5%"
+        sx={{ gap: 15 }}
+      >
+        <Typography
+          variant="h4"
+          align="center"
+          textAlign="left"
+          sx={{ width: '30%', alignSelf: 'flex-end' }}
+        >
+          Ready for a top tasting experience?
+        </Typography>
+        <Box sx={{ width: '100%' }}>
+          <Carousel />
+        </Box>
+      </Stack>
+
+      <Box sx={{ marginLeft: '10%' }}>
+        <StyledLink href="/restaurants" passHref>
+          <Button
+            variant="contained"
+            sx={{
+              backgroundColor: '#FF4C29',
+              '&:hover': {
+                backgroundColor: '#334756 ',
+              },
+            }}
+          >
+            I&apos;m ready!
+          </Button>
+        </StyledLink>
+      </Box>
     </main>
   );
 }
---
+++ src/components/Carousel/Carousel.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import React, { FC } from 'react';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const images = [
+  {
+    label: 'Your ultimate New York guide to temptations!',
+    imgPath: '/carousel1.png',
+  },
+  {
+    label: 'Check out details and reviews and do not miss a thing!',
+    imgPath: '/carousel2.png',
+  },
+  {
+    label: 'Create your own favorites list today!',
+    imgPath: '/carousel3.png',
+  },
+];
+
+export const Carousel: FC = () => {
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleStepChange = (step: number) => {
+    setActiveStep(step);
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AutoPlaySwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={activeStep}
+        onChangeIndex={handleStepChange}
+        enableMouseEvents
+      >
+        {images.map((step, index) => (
+          <div key={step.label}>
+            {Math.abs(activeStep - index) <= 2 ? (
+              <Box
+                component="img"
+                sx={{
+                  display: 'block',
+                  maxWidth: '80%',
+                  overflow: 'hidden',
+                  width: '100%',
+                }}
+                src={step.imgPath}
+                alt={step.label}
+              />
+            ) : null}
+          </div>
+        ))}
+      </AutoPlaySwipeableViews>
+    </Box>
+  );
+};
