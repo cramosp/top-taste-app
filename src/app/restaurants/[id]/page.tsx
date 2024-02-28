dc7120181b081cb94a144aa82c177980af7f3ebe
Cristina RP <cristinarp95@gmail.com>
2024-02-28 19:09:16 +0100

    Add details page functionality

--- src/app/restaurants/[id]/page.tsx
+++ src/app/restaurants/[id]/page.tsx
@@ -1,7 +1,41 @@
-export default async function RestaurantDetails() {
+import { API_URL } from '@/core/env';
+import { Restaurant } from '@/core/types';
+import { API_ROUTES } from '@/core/routes';
+
+export async function generateStaticParams() {
+  const restaurants: Restaurant[] = await fetch(
+    `${API_URL}${API_ROUTES.RESTAURANTS}`
+  ).then((res) => res.json());
+
+  return restaurants.map((restaurant) => ({
+    id: restaurant._id,
+  }));
+}
+
+async function getRestaurant(restaurantId: string): Promise<Restaurant> {
+  const res = await fetch(
+    `${API_URL}${API_ROUTES.RESTAURANTS}/${restaurantId}`
+  );
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch restaurant');
+  }
+
+  return res.json();
+}
+
+export default async function RestaurantDetails({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const restaurant = await getRestaurant(params.id);
+
   return (
     <main>
-      <h1>DETAILS</h1>
+      <div>
+        <h1>{restaurant.name}</h1>
+      </div>
     </main>
   );
 }
