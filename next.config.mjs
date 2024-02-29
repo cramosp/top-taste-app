4a58ca312c51c05c492321f3ab217d7510ea5146
Cristina RP <cristinarp95@gmail.com>
2024-02-29 17:36:42 +0100

    Improve Details page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      'media-cdn.tripadvisor.com',
+      'video-images.vice.com',
+      'www.livingfla.com',
+      'c8.alamy.com',
+      'pyxis.nymag.com',
+      'www.wazwu.com',
+      'loving-newyork.com',
+      'resizer.otstatic.com',
+      'i.pinimg.com',
+    ],
+  },
+};
 
 export default nextConfig;
